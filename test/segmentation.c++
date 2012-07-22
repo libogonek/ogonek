f8d3a0624a32cd59416b7d4a207706ffa25758d0
Martinho Fernandes <martinho.fernandes@gmail.com>
2012-07-22 14:37:44 +0100

    Adopting UCD tests.

--- test/segmentation.c++
+++ test/segmentation.c++
@@ -17,37 +17,29 @@
 #include <catch.h++>
 
 #include <string>
+#include <vector>
+
+struct grapheme_cluster_test {
+    std::u32string input;
+    std::vector<int> breaks;
+};
+
+grapheme_cluster_test grapheme_cluster_test_data[] = {
+    #include "grapheme_cluster_test.g.inl"
+};
 
 TEST_CASE("grapheme_clusters", "Extended grapheme cluster ranges") {
-    // Example clusters from UAX #29
-    std::u32string input = U"\x0067\x0308\xAC01\x1100\x1161\x11A8\x0BA8\x0BBF\x0E40\x0E01\x0E01\x0E33\x0937\x093F";
-    // expected result:      | 0067  0308| AC01| 1100  1161  11A8| 0BA8  0BBF| 0E40| 0E01| 0E01  0E33| 0937  093F|
-    auto clusters = ogonek::grapheme_clusters(input);
-    auto it = clusters.begin();
-    CHECK(it->begin() == input.begin());
-    CHECK(it->end()   == input.begin() + 2);
-    ++it;
-    CHECK(it->begin() == input.begin() + 2);
-    CHECK(it->end()   == input.begin() + 3);
-    ++it;
-    CHECK(it->begin() == input.begin() + 3);
-    CHECK(it->end()   == input.begin() + 6);
-    ++it;
-    CHECK(it->begin() == input.begin() + 6);
-    CHECK(it->end()   == input.begin() + 8);
-    ++it;
-    CHECK(it->begin() == input.begin() + 8);
-    CHECK(it->end()   == input.begin() + 9);
-    ++it;
-    CHECK(it->begin() == input.begin() + 9);
-    CHECK(it->end()   == input.begin() + 10);
-    ++it;
-    CHECK(it->begin() == input.begin() + 10);
-    CHECK(it->end()   == input.begin() + 12);
-    ++it;
-    CHECK(it->begin() == input.begin() + 12);
-    CHECK(it->end()   == input.end());
-    ++it;
-    CHECK(it == clusters.end());
+    for(auto&& test : grapheme_cluster_test_data) {
+        auto clusters = ogonek::grapheme_clusters(test.input);
+        auto it = clusters.begin();
+        auto last_break = 0;
+        for(auto&& this_break : test.breaks) {
+            CHECK(it->begin() == test.input.begin() + last_break);
+            CHECK(it->end()   == test.input.begin() + this_break);
+            last_break = this_break;
+            ++it;
+        }
+        CHECK(it == clusters.end());
+    }
 }
 
---
+++ tools/ucd2c++/SegmentationTestCompiler.cs
@@ -0,0 +1,100 @@
+// Ogonek
+//
+// Written in 2012 by Martinho Fernandes <martinho.fernandes@gmail.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and related
+// and neighboring rights to this software to the public domain worldwide. This software is
+// distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along with this software.
+// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+// Unicode segmentation tests compiler
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Xml.Linq;
+
+[assembly: AssemblyTitle("Ogonek.SegmentationTestCompiler")]
+[assembly: AssemblyDescription("Unicode segmentation test-to-C++ compiler")]
+[assembly: AssemblyProduct("Ogonek.SegmentationTestCompiler")]
+[assembly: AssemblyCopyright("Written in 2012 by Martinho Fernandes")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+namespace Ogonek.SegmentationTestCompiler
+{
+    static class Program
+    {
+        static int Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            if(args.Length != 3)
+            {
+                Console.WriteLine("Usage: segtest2c++ <test source> <output file> <description>");
+                return 1;
+            }
+            string source = args[0];
+            string destination = args[1];
+            string kind = args[2];
+
+            var tests = GetTests(File.ReadLines(source))
+                            .ToList();
+            var strings = tests.Select(GetString);
+            var breaks = tests.Select(GetBreaks);
+            File.WriteAllLines(destination, new []{ string.Format(CopyrightNotice, DateTime.Now, kind) });
+            File.AppendAllLines(destination, strings.Zip(breaks, (s,b) => "{ " + s + ", { " + string.Join(", ", b) + " } },"));
+
+            return 0;
+        }
+
+        static IEnumerable<string> GetTests(IEnumerable<string> lines) {
+            return lines.Where(l => !l.StartsWith("#"))
+                        .Select(l => l.Split('#')[0]);
+        }
+
+        static string GetString(string line) {
+            return "U\"" + string.Concat(
+                line.Split(new[]{ '×', '÷' })
+                    .Select(s => s.Trim())
+                    .Where(s => !string.IsNullOrWhiteSpace(s))
+                    .Select(s => s.Trim())
+                    .Select(s => @"\x" + s))
+                + "\"";
+        }
+
+        static IEnumerable<int> GetBreaks(string text) { 
+            return text.Where(c => c == '×' ||  c == '÷')
+                       .Select((c,i) => new{ c, i })
+                       .Where(x => x.c == '÷')
+                       .Skip(1)
+                       .Select(x => x.i);
+        }
+
+        const string CopyrightNotice = @"// Ogonek
+//
+// Written in 2012 by Martinho Fernandes <martinho.fernandes@gmail.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and related
+// and neighboring rights to this software to the public domain worldwide. This software is
+// distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along with this software.
+// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+// This file was automatically generated on {0}
+
+// Unicode segmentation test data - {1}
+";
+    }
+}
+
